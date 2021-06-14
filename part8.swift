f8797c1ab0373d26839c47f59a87617d4a1d9d3c
Samin Yeasar Abir <63140759+Y3454R@users.noreply.github.com>
2021-06-14 18:44:49 +0600

    Create part8.swift
    Structures

---
+++ part8.swift
@@ -0,0 +1,10 @@
+// Structures
+print("Structures:")
+struct CellPoint {
+    var x = 0.0
+    var y = 0.0
+}
+
+var a = CellPoint(x: 1.0, y: 2.0) ; var b = a // ek line e semicolon dile hoy
+b.x = 3.0
+print("a.x: \(a.x)")
